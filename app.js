88f334ee9bd151d7434a0f7466f5945e95204b69
Andrea Rossolini <andrearossolini.96@gmail.com>
2025-11-17 00:11:31 +0000

    adding changes to app to read metrics

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const client = require('prom-client');
 const app = express();
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+// Collect default metrics like CPU, memory, etc.
+collectDefaultMetrics({ prefix: 'node_app_' });
+
+
+// Expose the metrics endpoint
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
