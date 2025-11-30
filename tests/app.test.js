1570393ea720bd61fdc7f68f78b56831b243a773
Andrea Rossolini <andrearossolini.96@gmail.com>
2025-11-30 17:31:32 +0000

    demo

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,11 +2,11 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('GET /', () => {
-  it('should return 200 OK and "Hello, World!"', async () => {
+  it('should return 200 OK and "Hello, Demo!"', async () => {
     const res = await request(app)
       .get('/')
       .expect(200);
 
-    expect(res.text).toBe('Hello, World!');
+    expect(res.text).toBe('Hello, Demo!');
   });
 });
